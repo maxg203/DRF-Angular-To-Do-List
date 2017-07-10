904f554bdd5d3975ca2af95b33d7420186f92a32
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-07-10 22:31:07 +0100

    End of part 6

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,4 +1,8 @@
 var app = angular.module('toDo', []);
 app.controller('toDoController', function($scope) {
-    
+    $scope.todoList = [{todoText: 'Finish this app'}];
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText: $scope.todoInput});
+        $scope.todoInput = '';
+    };
 })
