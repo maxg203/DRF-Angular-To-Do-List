c16e2119895299844a999da8f9741a1a41d7b27a
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-07-11 22:34:37 +0100

    Added 'done' button to remove done items from the list

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,8 +1,15 @@
 var app = angular.module('toDo', []);
 app.controller('toDoController', function($scope) {
-    $scope.todoList = [{todoText: 'Finish this app'}];
+    $scope.todoList = [{todoText: 'Finish this app', done: false}];
     $scope.todoAdd = function() {
-        $scope.todoList.push({todoText: $scope.todoInput});
+        $scope.todoList.push({todoText: $scope.todoInput, done: false});
         $scope.todoInput = '';
     };
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x);
+        })
+    }
 })
