c6790cf3c485b93cf121c3c2f24e5d22737bd794
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-07-27 18:59:31 +0100

    Add to do model to keep a record of the to do list

--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'todo',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
