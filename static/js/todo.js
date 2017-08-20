5b1499dff07817ed23d05302656a05d1026e98ff
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-20 12:10:55 +0100

    Make first http GET request to custom API

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,6 +1,18 @@
 var app = angular.module('toDo', []);
-app.controller('toDoController', function($scope) {
-    $scope.todoList = [{todoText: 'Finish this app', done: false}];
+app.controller('toDoController', function($scope, $http) {
+    // $scope.todoList = [{todoText: 'Finish this app', done: false}];
+    $http.get('/todo/api/').then(function(response) {
+        $scope.todoList = [];
+        for (var i = 0; i < response.data.length; i++) {
+
+            var todo = {};
+            todo.todoText = response.data[i].text
+
+            todo.done = response.data[i].done
+            $scope.todoList.push(todo);
+        }
+        console.log(response.data);
+    });
     $scope.todoAdd = function() {
         $scope.todoList.push({todoText: $scope.todoInput, done: false});
         $scope.todoInput = '';
