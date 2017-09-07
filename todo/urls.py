8f7c3c8a463f2450f70c0b344df5b919b82d8c25
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-07 19:55:38 +0100

    Add detail view and DELETE support

--- todo/urls.py
+++ todo/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from todo.views import ToDoView
+from todo.views import ToDoListView, ToDoDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='todo/index.html')),
-    url(r'^todo/api/', ToDoView.as_view())
+    url(r'^todo/api/$', ToDoListView.as_view()),
+    url(r'^todo/api/(?P<pk>[0-9]+)/$', ToDoDetailView.as_view())
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,7 +8,7 @@ from todo.serializers import ToDoSerializer
 from todo.models import ToDo
 
 
-class ToDoView(APIView):
+class ToDoListView(APIView):
     def get(self, request):
         todos = ToDo.objects.all()
         serializer = ToDoSerializer(todos, many=True)
@@ -19,3 +21,15 @@ class ToDoView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ToDoDetailView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(ToDo, pk=pk)
+        serializer = ToDoSerializer(todo)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        todo = get_object_or_404(ToDo, pk=pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
